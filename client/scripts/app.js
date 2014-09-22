0c386a8ae7cb10902bf9957d5534a6a4a58acd14
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-09-22 14:24:39 -0700

    almost working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,10 +4,16 @@ var app = {
     this.server = 'https://api.parse.com/1/classes/chatterbox';
     this.fetch();
     $('#main').on('click', '.username', function(){
-      console.log(this)
       // console.log(this.text())
-      var username1 = this.text();
+      var username1 = $(this).text();
       app.addFriend(username1);
+    });
+    $('.submit').on('click', function(){
+      console.log('triggered') 
+      app.handleSubmit();
+    });
+    $('.update').on('click', function(){
+    app.addMessage(app.fetch());
     });
   },
   send: function(message){
@@ -45,13 +51,26 @@ var app = {
     $('#chats').children().remove();
   },
   addMessage: function(message){
-    var temp = "<div><div class='message'>"+message.text+"<div class='username'>"+message.user+"</div>";
+    var temp = "<div><div class='message'>"+message.text+"<div class='username'>"+message.username+"</div>";
     $('#chats').append(temp);
   },
   addRoom: function(room){
     var temp = "<div class="+room+">"+room+"</div>";
     $('#roomSelect').append(temp);
   },
-  addFriend: function(friend){}
+  addFriend: function(friend){},
+  handleSubmit: function(){
+    var messagepart = $('.text-message').text()
+    var userpart = String.prototype.slice.call(window.location.search, 10)
+    var message = {
+          username: userpart,
+          text: messagepart,
+          roomname: 'lobby'
+        };
+    app.send(message)
+  }
 
 };
+$(document).ready(function() {
+    app.init();
+});
