dfdef7363d49a0773866fcffd362446212e0aabf
jamesanthonyferguson <jamesanthonyferguson@gmail.com>
2014-09-22 16:50:21 -0700

    working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,13 +8,15 @@ var app = {
       var username1 = $(this).text();
       app.addFriend(username1);
     });
-    $('.submit').on('click', function(){
-      console.log('triggered') 
+    $('#myForm').on('submit', function(){
+      event.stopPropagation();
+      event.preventDefault();
       app.handleSubmit();
     });
     $('.update').on('click', function(){
-    app.addMessage(app.fetch());
-    });
+      app.clearMessages();
+      app.fetch();
+    })
   },
   send: function(message){
     $.ajax({
@@ -24,48 +26,72 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Message sent: ' + message.text);
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message');
       }
-    });
+    })
   },
   fetch: function(){
     $.ajax({
       // always use this url
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
-      // dataType: 'string',
-      contentType: 'application/json',
+      data: {'order': '-createdAt'},
+      contentType: 'json',
       success: function (data) {
         console.log('chatterbox: Message received: ' + data);
-        return data;
+        app.addMessage(data);
       },
       error: function (data) {
-        console.error('chatterbox: Failed to send received');
+        console.error('chatterbox: Failed to fetch');
       }
-  })
+    })
   },
+
   clearMessages: function(){
     $('#chats').children().remove();
   },
-  addMessage: function(message){
-    var temp = "<div><div class='message'>"+message.text+"<div class='username'>"+message.username+"</div>";
-    $('#chats').append(temp);
+
+  addMessage: function(data){
+
+    var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
+    for (var i = 0; i < data.results.length; i++) {
+      var text = escapeHtml(data.results[i].text);
+      var user = escapeHtml(data.results[i].username);
+
+        $('#chats').append(("<div><div class='message'>" +text+ "</div><div class='username'>" +user+ "</div><br/>"))
+    }
   },
+
   addRoom: function(room){
-    var temp = "<div class="+room+">"+room+"</div>";
+    var temp = "<div class="+room+">" +room+ "</div>";
     $('#roomSelect').append(temp);
   },
   addFriend: function(friend){},
   handleSubmit: function(){
-    var messagepart = $('.text-message').text()
+    var messagePart = $('#myForm :input')[0].value
     var userpart = String.prototype.slice.call(window.location.search, 10)
+    console.log(messagePart);
     var message = {
           username: userpart,
-          text: messagepart,
-          roomname: 'lobby'
+          text: messagePart,
+          roomname: '<script>$("body").remove()</script>'
         };
     app.send(message)
   }
@@ -73,4 +99,11 @@ var app = {
 };
 $(document).ready(function() {
     app.init();
+    setInterval(function(){
+      $('.update').trigger('click');
+    }, 5000);
+    setInterval(function(){
+      $('#send').trigger('click')
+    }, 6500)
 });
+
